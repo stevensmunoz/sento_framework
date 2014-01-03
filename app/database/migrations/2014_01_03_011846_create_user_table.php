12c496fa95bfea114293101544a5b992d5f9e625
Stiven Castillo <stivencastillo.90@gmail.com>
2014-01-02 22:43:34 -0500

    Config Laravel

---
+++ app/database/migrations/2014_01_03_011846_create_user_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		/* Tabla estado de usuario administrador */
+		Schema::create('user_estado', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('nombre');
+			$table->timestamps();
+		});
+		/* Usuario administrador del sistema */
+		Schema::create('user', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('nombre');
+			$table->string('usuario');
+			$table->string('password');
+			$table->string('email');
+			$table->integer('id_estado')->unsigned();
+			$table->foreign('id_estado')->references('id')->on('user_estado') ->onDelete('cascade');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('user_estado');
+		Schema::drop('user');
+	}
+
+}
